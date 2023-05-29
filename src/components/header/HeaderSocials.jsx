10f755deb808358db60bc4e25de69aad9a19b3e0
Bobgala Harrison <67080601+HarryShots@users.noreply.github.com>
2023-05-29 16:43:31 +0100

    updated header

--- src/components/header/HeaderSocials.jsx
+++ src/components/header/HeaderSocials.jsx
@@ -1,14 +1,14 @@
 import React from 'react'
 import {BsLinkedin} from 'react-icons/bs'
 import {BsGithub} from 'react-icons/bs'
-import {BsDribbble} from 'react-icons/bs'
+import {BsInstagram} from 'react-icons/bs'
 
 const HeaderSocials = () => {
   return (
     <div className='header__socials'>
-        <a href="https://linkedin.com" target='_blank'><BsLinkedin /></a>
-        <a href="https://github.com" target='_blank'><BsGithub /></a>
-        <a href="https://dribbble.com" target='_blank'><BsDribbble /></a>    
+        <a href="https://www.linkedin.com/in/bobgala-harrison-525568198/" target='_blank'><BsLinkedin /></a>
+        <a href="https://www.instagram.com/harryshots_photography/" target='_blank'><BsInstagram /></a>
+        <a href="https://github.com/HarryShots" target='_blank'><BsGithub /></a>    
     </div>
   )
 }
