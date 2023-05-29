be806acd941432753332f8c76ebd1578c3f62531
Bobgala Harrison <67080601+HarryShots@users.noreply.github.com>
2023-05-29 17:31:25 +0100

    updated testimonials section

--- src/components/testimonials/Testimonials.jsx
+++ src/components/testimonials/Testimonials.jsx
@@ -18,22 +18,22 @@ const data = [
   {
     avatar: AVTR1,
     name: 'Kessy Durky',
-    review: 'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Beatae illo eius quis, omnis rem iste similique consequatur deleniti dolorem vero? Itaque, nam suscipit? Alias repudiandae voluptate maxime quam, expedita officia.'
+    review: 'I was so happy with the photos that HarryShots took of my wedding. He captured the day perfectly and I will cherish him forever.'
   },
   {
     avatar: AVTR2,
     name: 'Brenda',
-    review: 'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Beatae illo eius quis, omnis rem iste similique consequatur deleniti dolorem vero? Itaque, nam suscipit? Alias repudiandae voluptate maxime quam, expedita officia.'
+    review: 'HarryShots is a true artist. He has a great eye for composition and lighting, and he knows how to capture the beauty in every moment.'
   },
   {
     avatar: AVTR3,
     name: 'Malando Tina',
-    review: 'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Beatae illo eius quis, omnis rem iste similique consequatur deleniti dolorem vero? Itaque, nam suscipit? Alias repudiandae voluptate maxime quam, expedita officia.'
+    review: 'I was very happy with the work that HarryShots did on my website. He was able to take my vision and turn it into a reality. The website is exactly what I wanted and it looks great. I would definitely recommend HarryShots to anyone who is looking for a web developer.'
   },
   {
     avatar: AVTR4,
     name: 'Makoun Glory',
-    review: 'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Beatae illo eius quis, omnis rem iste similique consequatur deleniti dolorem vero? Itaque, nam suscipit? Alias repudiandae voluptate maxime quam, expedita officia.'
+    review: 'Harry was very easy to work with. He was always responsive to my feedback and he was always willing to make changes to the content until I was happy with the results.'
   }
 ]
 
